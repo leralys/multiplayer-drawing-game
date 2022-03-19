43840a18b5ce37b5bf62dd491d4072405f108870
leralys <valerialysko@gmail.com>
2022-03-19 11:40:06 +0200

    rooms, server-side

--- server/app.js
+++ server/app.js
@@ -21,29 +21,42 @@ const io = new Server(httpServer, {
 
 let clientNum = 0;
 
-const connected = (socket) => {
-    socket.on('newPlayer', (data) => {
+const connected = socket => {
+    socket.on('newPlayer', data => {
         const msg = playersController.addNewPlayer(data);
         if (!msg.status) {
-            // username taken
-            io.to(data.socketId).emit('notifyPlayer', msg);
+            // username taken - notify player
+            io.to(data.socketId).emit('notifyPlayer', { msg });
         } else {
             //username doesn't exist - may connect
             clientNum++;
             // send a player to a room where only two players can be
             let roomNo = Math.round(clientNum / 2);
             socket.join(roomNo);
-            // notify a user that he is connected successfully , and his roomNo
-            io.to(data.socketId).emit('notifyPlayer', msg, roomNo);
+            let turn = 0;
+            // first player in a room
+            clientNum % 2 === 1 ? turn = 1 : turn = 2;
+            // notify a player that he is connected successfully, his roomNo, and his turn
+            io.to(data.socketId).emit('notifyPlayer', { msg, roomNo, turn });
         }
     });
+    // when second player comes to a room, notify the first one that the game starts
+    socket.on('startNewGame', ({ roomNo, username }) => {
+        socket.to(roomNo).emit('startNewGame', { startNewGame: true });
+        //and send info about the second user the room
+        socket.to(roomNo).emit('opponentInfo', username);
+    });
+    socket.on('some event', ({ username, roomNo }) => {
+        // socket.to(roomNo).emit('getOpponent ', username);
+        roomNo && socket.to(roomNo).emit('opponentInfo', username);
+    });
+    // notify other players when leaving the room
     socket.on('disconnecting', () => {
         // socket.rooms is a Set which contains at least the socket ID
-        // notify other players in the room upon leaving the room
         for (let room of socket.rooms) {
             const leavingPlayer = playersController.getDisconnectingPlayer(socket.id);
             if (leavingPlayer) {
-                io.to(room).emit('notifyPlayer', leavingPlayer.username);
+                io.to(room).emit('leavingPlayer', leavingPlayer.username);
             }
         }
     });
@@ -53,28 +66,7 @@ const connected = (socket) => {
     });
 }
 
-// io.on('connection', (socket) => {
-//     // console.log(socket.id);
-//     io.emit('serverToClient', 'hello from the server');
-//     socket.on('clientToServer', msg => {
-//         console.log(msg);
-//     });
-//     socket.on('clientToClient', msg => {
-//         socket.broadcast.emit('serverToClient', msg);
-//     });
-//     socket.on('disconnectMe', msg => {
-//         console.log(msg);
-//     });
-//     socket.on('disconnect', () => {
-//         console.log(socket.id + ' left');
-//     });
-// });
-
 io.on('connection', connected);
-
-// app.get('/', (req, res) => {
-//     res.send('hello world');
-// });
 
 httpServer.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
